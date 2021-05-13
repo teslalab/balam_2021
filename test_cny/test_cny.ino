4dca70daf7e810becb028d6cd2b55613303207a8
Gabriel Monzon <gabriel.monzon39@gmail.com>
2021-05-13 12:35:31 -0600

    test cny

--- test_cny/test_cny.ino
+++ test_cny/test_cny.ino
@@ -6,14 +6,33 @@ void setup()
 
 void loop()
 {
-    Serial.print("Sensor0: ");
-    Serial.println(analogRead(36));
-    Serial.print("Sensor1: ");
-    Serial.println(analogRead(34));
-    Serial.print("Sensor2: ");
-    Serial.println(analogRead(39));
-    Serial.print("Sensor3: ");
-    Serial.println(analogRead(35));
+
+    int s0 = analogRead(36);
+    int s1 = analogRead(34);
+    int s2 = analogRead(39);
+    int s3 = analogRead(35);
+
+
+    // Serial.print("Sensor0: ");
+    // Serial.println(s0);
+    // Serial.print("Sensor1: ");
+    // Serial.println(s1);
+    // Serial.print("Sensor2: ");
+    // Serial.println(s2);
+    // Serial.print("Sensor3: ");
+    // Serial.println(s3);
+
+    (s0 < 1500) ? s0 = 1 : s0 = 0;
+    (s1 < 1500) ? s1 = 1 : s1 = 0;
+    (s2 < 1500) ? s2 = 1 : s2 = 0;
+    (s3 < 1500) ? s3 = 1 : s3 = 0;
+    
+
+    Serial.print(s3);
+    Serial.print(s2);
+    Serial.print(s1);
+    Serial.println(s0);
+    
 
     delay(10);
 }
