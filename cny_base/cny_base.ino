c6222bc5ae1bfc81257e57b3fbe934b203d8ada2
Gabriel Monzon <gabriel.monzon39@gmail.com>
2021-05-18 13:40:27 -0600

    rename CNY

--- cny_base/cny_base.ino
+++ cny_base/cny_base.ino
@@ -1,7 +1,28 @@
+
+#include <TB6612_ESP32.h>
+
+//MOTOR1
+#define AIN1 32
+#define AIN2 33
+#define PWMA 12
+
+//MOTOR2
+#define BIN1 25
+#define BIN2 26
+#define PWMB 14
+
+const int offsetA = 1;
+const int offsetB = -1;
+
+Motor motor1 = Motor(AIN1, AIN2, PWMA, offsetA, 0, 5000, 8, 1);
+Motor motor2 = Motor(BIN1, BIN2, PWMB, offsetB, 0, 5000, 8, 2);
+
+const int velocidad = 50;
+const int giro = 100;
+
 void setup()
 {
     Serial.begin(115200);
-    Serial.println("Comenzando test");
 }
 
 void loop()
@@ -11,7 +32,6 @@ void loop()
     int s1 = analogRead(34);
     int s2 = analogRead(39);
     int s3 = analogRead(35);
-
 
     // Serial.print("Sensor0: ");
     // Serial.println(s0);
@@ -33,6 +53,18 @@ void loop()
     Serial.print(s1);
     Serial.println(s0);
     
+    if (s0 == 0)
+    {
+        forward(motor2, motor2, giro);
+    }
+    else if (s3 == 0)
+    {
+        forward(motor1, motor1, giro);
+    }else{
 
-    delay(10);
+        forward(motor1, motor2, velocidad);
+    }
+    
+
+    delay(50);
 }
